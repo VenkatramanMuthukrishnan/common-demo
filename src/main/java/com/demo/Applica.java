8da3df3ce078b73d75f848660211d5bbe1e64518
Venkatraman Muthukrishnan <vmuthukrishnan@informatica.com>
2024-11-21 13:37:38 +0530

    vulnerable

--- src/main/java/com/demo/Applica.java
+++ src/main/java/com/demo/Applica.java
@@ -1,8 +1,12 @@
 package com.demo;
 
+import org.apache.avro.AvroTypeException;
+
 public class Applica {
 
-    public static void main(String[] args) {
-        System.out.printf("hello");
-    }
+   public static void run(int a){
+       if(a<0) {
+           throw new AvroTypeException("test");
+       }
+   }
 }
